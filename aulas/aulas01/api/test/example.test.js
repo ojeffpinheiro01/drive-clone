0d897f8935e2bb0682ea2b74e02c7d0568240287
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-09-06 15:55:08 -0300

    configurando servidor

---
+++ aulas/aulas01/api/test/example.test.js
@@ -0,0 +1,11 @@
+import { 
+    describe,
+    test,
+    expect
+ } from '@jest/globals'
+
+ describe('#Testando', () => {
+     test('#teste', () => {
+         expect(true).toBeTruthy()
+     })
+ })
